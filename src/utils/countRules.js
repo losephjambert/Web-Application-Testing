a86f8dcb0fdd2112fb23d3518cc67f9225143055
Joe <josephc.lambert@gmail.com>
2019-10-03 14:52:24 -0700

    stubs count rules

---
+++ src/utils/countRules.js
@@ -0,0 +1,19 @@
+export const handleBall = () => {
+  // increase the current ball count by 1
+  // if the current ball count === 3, reset current ball count to 0
+};
+
+export const handleStrike = () => {
+  // increase the current strike count by 1
+  // if the current strike count === 2, reset current strike count to 0
+};
+
+export const handleFoul = () => {
+  // if the current strike count is < 2, increase the current strike count by 1
+  // if the current strike count is >= 2, do nothing
+};
+
+export const handleHit = () => {
+  // reset current ball count
+  // resest current strike count
+};
