4a906b98593d99838a1d7ed45161f500ed5ac455
Joe <josephc.lambert@gmail.com>
2019-10-03 15:04:24 -0700

    builds out coutn rules functions

--- src/utils/countRules.js
+++ src/utils/countRules.js
@@ -1,19 +1,30 @@
-export const handleBall = () => {
+const resetCount = () => {
+  return {
+    balls: 0,
+    strikes: 0,
+  };
+};
+
+export const handleBall = ballCount => {
   // increase the current ball count by 1
   // if the current ball count === 3, reset current ball count to 0
+  return ballCount < 3 ? (ballCount += 1) : resetCount();
 };
 
-export const handleStrike = () => {
+export const handleStrike = strikeCount => {
   // increase the current strike count by 1
   // if the current strike count === 2, reset current strike count to 0
+  return strikeCount < 2 ? (strikeCount += 1) : resetCount();
 };
 
-export const handleFoul = () => {
+export const handleFoul = strikeCount => {
   // if the current strike count is < 2, increase the current strike count by 1
   // if the current strike count is >= 2, do nothing
+  return strikeCount < 1 ? (strikeCount += 1) : strikeCount;
 };
 
 export const handleHit = () => {
   // reset current ball count
   // resest current strike count
+  resetCount();
 };
