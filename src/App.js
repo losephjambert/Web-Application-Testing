51b8e0a4d44f40572cc9cdd7fafd4ec99f1cdb7a
Joe <josephc.lambert@gmail.com>
2019-10-03 14:33:30 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Baseball Rules</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
